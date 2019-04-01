a2aefffe0224c1a10bb4246c6e60d04efd6d19b6
pandeyavishek <pandeyavishek@users.noreply.github.com>
2019-04-01 13:56:11 -0400

    updates

---
+++ LectureJava/src/Lecture20Mar27.java
@@ -0,0 +1,5 @@
+public class Lecture20Mar27 {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ LectureJava/src/myInitals.java
@@ -0,0 +1,6 @@
+public class myInitals {
+    public static void main(String[] args) {
+        System.out.println("  A   ");
+        System.out.println(P);
+    }
+}
