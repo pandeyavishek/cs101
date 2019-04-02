c4663679993ee250d2ceb895950a2c377cccd3c7
pandeyavishek <pandeyavishek@users.noreply.github.com>
2019-04-02 15:24:31 -0400

    new final version of assignment 9

---
+++ docs/CS101Java/src/A9_ap9sa_Avishek_Pandey_complete.java
@@ -0,0 +1,40 @@
+public class A9_ap9sa_Avishek_Pandey_complete {
+    public static void main(String[] args) {
+        String color = "";
+        String slenderOrFat = "";
+        String usedOrNew = "";
+        String outputGood = "It's me! It's me! It's me!";
+        String outputBad = "Oh shucks";
+
+        if (color.equals("White") && slenderOrFat.equals("Fat") && usedOrNew.equals("Used")) {
+            System.out.println(outputGood);
+        } else {
+            System.out.println(outputBad);
+        }
+
+//        while (cloverFound == false && honeybeeLives) {
+//            System.out.println("Enter a East/West coordinate:");
+//            westEast = in.nextInt();
+//
+//
+//            System.out.println("Enter a North/South coordinate");
+//            northSouth = in.nextInt();
+//            //System.out.println("You entered: " + westEast);
+//
+//            if (Math.sqrt(Double.sum(westEast * westEast, northSouth * northSouth)) > MAX_BEE_FLIGHT_UNITS) {
+//                System.out.println("I failed...");
+//                honeybeeLives = false;
+//            } else {
+//                honeybeeWestEast = westEast + honeybeeWestEast;
+//                honeybeeNorthSouth = northSouth + honeybeeNorthSouth;
+//                System.out.println("Honeybee is at (" + honeybeeWestEast + "," + honeybeeNorthSouth + ")");
+//
+//                if (honeybeeWestEast == cloverWestEast && honeybeeNorthSouth == cloverNorthSouth) {
+//                    System.out.println("I found the clover, THANK YOU!");
+//                    cloverFound = true;
+//                }
+//            }
+//
+//        }
+    }
+}
