6d38663d832b005ccdd4d1b1d814bf530a225fdb
pandeyavishek <pandeyavishek@users.noreply.github.com>
2019-04-02 19:59:56 -0400

    Final

--- docs/CS101Java/src/A9_ap9sa_Avishek_Pandey_complete.java
+++ docs/CS101Java/src/A9_ap9sa_Avishek_Pandey_complete.java
@@ -1,3 +1,10 @@
+//Avishek Pandey ap9sa
+//CS 1010
+//Problem Set 9
+//April 2, 2019
+
+import java.util.Scanner;
+
 public class A9_ap9sa_Avishek_Pandey_complete {
     public static void main(String[] args) {
         String color = "";
@@ -6,35 +13,22 @@ public class A9_ap9sa_Avishek_Pandey_complete {
         String outputGood = "It's me! It's me! It's me!";
         String outputBad = "Oh shucks";
 
+        Scanner in = new Scanner(System.in);
+
+        System.out.println("What color?");
+        color = in.next();
+
+        System.out.println("Slender or Fat?");
+        slenderOrFat = in.next();
+
+        System.out.println("Used or New?");
+        usedOrNew = in.next();
+
         if (color.equals("White") && slenderOrFat.equals("Fat") && usedOrNew.equals("Used")) {
             System.out.println(outputGood);
         } else {
             System.out.println(outputBad);
         }
 
-//        while (cloverFound == false && honeybeeLives) {
-//            System.out.println("Enter a East/West coordinate:");
-//            westEast = in.nextInt();
-//
-//
-//            System.out.println("Enter a North/South coordinate");
-//            northSouth = in.nextInt();
-//            //System.out.println("You entered: " + westEast);
-//
-//            if (Math.sqrt(Double.sum(westEast * westEast, northSouth * northSouth)) > MAX_BEE_FLIGHT_UNITS) {
-//                System.out.println("I failed...");
-//                honeybeeLives = false;
-//            } else {
-//                honeybeeWestEast = westEast + honeybeeWestEast;
-//                honeybeeNorthSouth = northSouth + honeybeeNorthSouth;
-//                System.out.println("Honeybee is at (" + honeybeeWestEast + "," + honeybeeNorthSouth + ")");
-//
-//                if (honeybeeWestEast == cloverWestEast && honeybeeNorthSouth == cloverNorthSouth) {
-//                    System.out.println("I found the clover, THANK YOU!");
-//                    cloverFound = true;
-//                }
-//            }
-//
-//        }
     }
 }
