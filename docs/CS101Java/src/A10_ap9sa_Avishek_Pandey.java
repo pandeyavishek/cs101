c5abc775b58860f9b98384a0956fa42e2f2a8c70
pandeyavishek <pandeyavishek@users.noreply.github.com>
2019-04-10 13:36:25 -0400

    final update

--- docs/CS101Java/src/A10_ap9sa_Avishek_Pandey.java
+++ docs/CS101Java/src/A10_ap9sa_Avishek_Pandey.java
@@ -1,12 +1,15 @@
-//Avishek Pandey ap9sa
-//CS 1010
-//April 1, 2019
-//Problem Set 10
+/**
+ * Avishek Pandey ap9sa
+ * CS 1010
+ * April 1, 2019
+ * Problem Set 10
+ */
 
 import java.util.Scanner;
 
 public class A10_ap9sa_Avishek_Pandey {
     public static void main(String[] args) {
+        //Declaring variables
         int MAX_BEE_FLIGHT_UNITS = 5;
         int honeybeeWestEast = 0;
         int honeybeeNorthSouth = 0;
@@ -17,28 +20,29 @@ public class A10_ap9sa_Avishek_Pandey {
         String output = "Beeboss commands all bees to fly to the clover field at (" + cloverWestEast + "," + cloverNorthSouth + ')';
         boolean cloverFound = false;
         boolean honeybeeLives = true;
-        Scanner in = new Scanner(System.in);
+        Scanner in = new Scanner(System.in); //Scanner for user input
 
-        System.out.println(output);
+        System.out.println(output); //prints the Beeboss command and location of clover
 
-        while (cloverFound == false && honeybeeLives) {
+        while (cloverFound == false && honeybeeLives) {     //while loop for east/west coordinate
             System.out.println("Enter a East/West coordinate:");
-            westEast = in.nextInt();
+            westEast = in.nextInt(); //gathers e/w coordinate
 
 
-            System.out.println("Enter a North/South coordinate");
+            System.out.println("Enter a North/South coordinate"); //gathers the n/s coordinate
             northSouth = in.nextInt();
-            //System.out.println("You entered: " + westEast);
 
+
+            //If statement for checking whether bee dies for going over distance of 5
             if (Math.sqrt(Double.sum(westEast * westEast, northSouth * northSouth)) > MAX_BEE_FLIGHT_UNITS) {
                 System.out.println("I failed...");
                 honeybeeLives = false;
             } else {
                 honeybeeWestEast = westEast + honeybeeWestEast;
                 honeybeeNorthSouth = northSouth + honeybeeNorthSouth;
-                System.out.println("Honeybee is at (" + honeybeeWestEast + "," + honeybeeNorthSouth + ")");
+                System.out.println("Honeybee is at (" + honeybeeWestEast + "," + honeybeeNorthSouth + ")"); //reports location of bee
 
-                if (honeybeeWestEast == cloverWestEast && honeybeeNorthSouth == cloverNorthSouth) {
+                if (honeybeeWestEast == cloverWestEast && honeybeeNorthSouth == cloverNorthSouth) { //sees if be location = clover location
                     System.out.println("I found the clover, THANK YOU!");
                     cloverFound = true;
                 }
