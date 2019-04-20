9ef60b3ecfb97717d79887357d3c071d92678385
pandeyavishek <pandeyavishek@users.noreply.github.com>
2019-04-20 15:19:26 -0400

    Updated 4/20

--- docs/CS101Java/src/A11_ap9sa_Avishek_Pandey.java
+++ docs/CS101Java/src/A11_ap9sa_Avishek_Pandey.java
@@ -28,6 +28,31 @@ public class A11_ap9sa_Avishek_Pandey {
             input = in.nextInt();
         }
 
+        interestRate = input / 100;
+        amount = INITAL_AMOUNT;
+
+        while (amount < 2 * INITAL_AMOUNT) {// idk what to do here
+
+            year++;
+            amount += amount * interestRate;
+        }
+
+        System.out.println("Enter years for investment to double as INTEGER between 1 & 72:");
+        input = in.nextInt();
+
+        while (input < 1 || input > 72) { //greater 72 or less than 1 get changed
+            System.out.println("Years input must be between 1 and 72 inclusive!");
+            System.out.println("Enter years for investment to double as INTEGER between 1 & 72:");
+            input = in.nextInt();
+        }
+
+        yearsToDouble = (int) input;
+        amount = INITAL_AMOUNT;
+        interestRate = 0.0;
+
+        while (amount < 2 * INITAL_AMOUNT) {
+
+        }
 
     }
 }
